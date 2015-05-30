aa1bf9cd93b8e4948c0a79aedcbaaa269a4f9bd7
ianrae <winfell808@gmail.com>
2015-05-30 16:30:00 -0400

    ref

--- test/mesf/ErrorTrackerTests.java
+++ test/mesf/ErrorTrackerTests.java
@@ -1,7 +1,7 @@
 package mesf;
 
 import static org.junit.Assert.*;
-import mesf.log.Logger;
+import mesf.errortracker.DefaultErrorTracker;
 
 import org.junit.Test;
 import org.mef.framework.sfx.ISfxErrorListener;
@@ -10,61 +10,10 @@ import org.mef.framework.sfx.SfxContext;
 
 public class ErrorTrackerTests extends BaseMesfTest
 {
-	public interface IErrorListener
-	{
-		void onError(String errMsg);
-	}
-	
-	public interface IErrorTracker
-	{
-		void setListener(IErrorListener listener);
-		public void errorOccurred(String errMsg);
-		public int getErrorCount();
-		public String getLastError();
-	}
-	
-	public class ErrorTracker implements IErrorTracker
-	{
-		private int errorCount;
-		private String lastError;
-		private IErrorListener listener;
-
-		@Override
-		public synchronized void setListener(IErrorListener listener) 
-		{
-			this.listener = listener;
-		}
-
-		@Override
-		public synchronized void errorOccurred(String errMsg) 
-		{
-			lastError = errMsg;
-			errorCount++;
-			Logger.log("ERROR: " + errMsg);
-			if (listener != null)
-			{
-				listener.onError(errMsg);
-			}			
-		}
-
-		@Override
-		public int getErrorCount() 
-		{
-			return errorCount;
-		}
-
-		@Override
-		public synchronized String getLastError() 
-		{
-			return this.lastError;
-		}
-		
-	}
-	
 	@Test
 	public void test() 
 	{
-		ErrorTracker tracker = new ErrorTracker();
+		DefaultErrorTracker tracker = new DefaultErrorTracker();
 		assertEquals(0, tracker.getErrorCount());
 		
 		tracker.errorOccurred("oops");
