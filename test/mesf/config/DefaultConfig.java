8dedeaa054103de3431fa8a01ee31d37d742db26
ianrae <winfell808@gmail.com>
2015-05-31 12:11:37 -0400

    config

---
+++ test/mesf/config/DefaultConfig.java
@@ -0,0 +1,28 @@
+package mesf.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DefaultConfig implements IConfig
+{
+	private Map<String, Integer> intMap = new HashMap<>();
+	
+	public DefaultConfig()
+	{
+		intMap.put(ConfigItem.COMMIT_CACHE_SEGMENT_SIZE.name(), 4);
+		intMap.put(ConfigItem.EVENT_CACHE_SEGMENT_SIZE.name(), 4);
+		intMap.put(ConfigItem.STREAM_CACHE_SEGMENT_SIZE.name(), 4);
+	}
+
+	@Override
+	public int getIntValue(ConfigItem item) 
+	{
+		Integer n = intMap.get(item);
+		if (n == null)
+		{
+			return Integer.MIN_VALUE;
+		}
+		return n;
+	}
+
+}
